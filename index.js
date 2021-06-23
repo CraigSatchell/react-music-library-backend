8b84d53a09e12c62b04c917b079d0a17860c1c41
Craig Satchell <satchdev@gmail.com>
2021-06-23 18:57:51 -0400

    add api routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
 const cors = require('cors');
+const {validateSong} = require('./middleware/songs-validation');
+
 
 const app = express();
 
@@ -9,6 +11,38 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.send('hello');
+});
+
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
+
+app.get('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+
+app.post('/api/songs', [validateSong], (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
+});
+
+app.put('/api/songs/:id', [validateSong], (req, res) => {
+    const id = req.params.id;
+    const songPropertiesToUpdate = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
+    return res.send(updatedSong)
+});
+
+
+app.delete('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.songs.deleteSong(id);
+    return res.send(updatedDataSet);
 });
 
 
